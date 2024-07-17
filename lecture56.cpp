4b5e708e06db052bc3a70c9b82c50a62d53461e4
yadavakriti03 <120010936+yadavakriti03@users.noreply.github.com>
2024-07-17 18:58:10 +0530

    Create lecture56.cpp

---
+++ lecture56.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+class node
+{
+public:
+   int data;
+   node *next;
+   node(int data_input)
+   {
+      this->data = data_input;
+      this->next = NULL;
+   }
+
+} void printnode(node *&node)
+{
+   cout << "Value is " << node->data;
+   cout << "address " << node->next;
+}
+void printlinkedlist(node *&head)
+{
+   if (head == NULL)
+   {
+      cout << "empty";
+      return;
+   }
+   node *temp = head;
+   while (temp != NULL)
+   {
+      cout << temp->data << " ";
+      temp = temp->next;
+   }
+}
+int main()
+{
+   node *head;
+   printlinkedlist(head);
+   node *node1 = new node(3);
+   node *head = node1;
+   node *node2 = new node(4);
+   node *node3 = new node(5);
+
+   // cout<<node1->data;
+   printnode(node1);
+   printnode(node2);
+   printnode(node3);
+
+   node1->next = node2;
+   node2->next = node3;
+
+   node *head = node1;
+   printlinkedlist(head);
+}
