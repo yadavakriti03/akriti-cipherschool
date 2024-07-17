3934862070bc075942dffa65e20588c729d0fb57
yadavakriti03 <120010936+yadavakriti03@users.noreply.github.com>
2024-07-17 18:43:57 +0530

    Create lecture45.cpp

---
+++ lecture45.cpp
@@ -0,0 +1,10 @@
+// greedy algorithm
+approaching a problem in aggression 
+//greedy approach   : focusing on that particular instant to find the right solution independent of what happens after
+A greedy algorithm is an algorithmic paradigm that follows the problem solving heuristic of making locally optimal choices at each stage with the hope of finding a global optimum. In other words, it is a method for solving an optimization problem by always choosing the best immediate option, without regard for future consequences.
+
+eg :-to cover maximum no of places 
+               |
+going to the nearest places to the current location
+they are often intuitive easy to understand and easy to mimplement 
+greedy solution is not necessarily best bcoz it not solve every problem
