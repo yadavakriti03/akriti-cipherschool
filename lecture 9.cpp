34a62ff536f53f6076449b72c3ae7df9c54564e4
yadavakriti03 <120010936+yadavakriti03@users.noreply.github.com>
2024-07-17 14:35:33 +0530

    Create lecture 9.cpp

---
+++ lecture 9.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main(){
+	for (int i = 0;i<5;i++){   // By using the for loop print the numbers from 0 to 4.
+		cout<<i<<endl;
+	}
+	return 0;
+}
+
+#include<iostream>
+using namespace std;
+int main(){
+	for(int i=0; i<=100;i++){ //here by using the for loop print 100 numbers
+		cout<<i<<endl;
+	}
+	return 0;
+}
+
+#include<iostream>
+using namespace std;
+int main(){
+	for(int i=0; i<100;i=i+2){ //here by using the for loop print the even numbers upto 100
+		cout<<i<<endl;
+	}
+	return 0;
+}
