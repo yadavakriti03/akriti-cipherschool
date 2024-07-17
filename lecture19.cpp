5ecba40675402360498de69d4f42a1a4c614abfe
yadavakriti03 <120010936+yadavakriti03@users.noreply.github.com>
2024-07-17 14:43:16 +0530

    Create lecture19.cpp

---
+++ lecture19.cpp
@@ -0,0 +1,12 @@
+//Address of an elements.
+#include <iostream>
+using namespace std;
+int main(){
+	int a=5;
+	float b=6;
+	long long c=6;
+	long long int d=7;
+	cout<<"The size of int is "<<sizeof(a)<<"The size of float is "<<sizeof(b)<<"the size of long long  is "<<sizeof(c);
+	cout<<"The size of long long int "<<sizeof(d);
+	return 0;
+}
