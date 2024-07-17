6f4a10283b45ab12fdafb4b21245d58f222dd0d5
yadavakriti03 <120010936+yadavakriti03@users.noreply.github.com>
2024-07-17 14:38:40 +0530

    Create lecture13.cpp

---
+++ lecture13.cpp
@@ -0,0 +1,6 @@
+In this lecture : Introduction of functions and what is meant by functions and how to use in c++.
+Actually Functions : Functions are used to perform certain actions, and they are important for reusing code .Define the code once, and use it many times.
+
+Functions in c++ = create a function
+C++ provides some pre-defined functions, such as main(), which is used to execute code. But you can also create your own functions to perform certain actions.
+Ex: int main() { block of code ...} // here main is the name of the function and here int means which they having return value. inside the brackets add the code that defines what the function should do
