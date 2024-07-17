0ecea35f713fc8cd504875a3f6a2f52af9a69d33
yadavakriti03 <120010936+yadavakriti03@users.noreply.github.com>
2024-07-17 18:46:30 +0530

    Create lecture51.cpp

---
+++ lecture51.cpp
@@ -0,0 +1,33 @@
+// CLASS and OBJECTS
+#include <iostream>
+using namespace std;
+class student
+{ // versatile datatype which can store multiple data types
+public:
+   string name;
+   int mids;
+   float ends;
+   bool pre_abs;
+};
+class vehicle
+{
+public:
+   string car_name;
+   string tyre_type;
+   int number_of_tyres;
+   int Number_of_seats;
+
+} int main()
+{
+
+   // these were class comes when you have lots of data to store that have same properties
+   //  string name;
+   //  int mid;
+   //  float ends;
+   //  bool pre_abs;
+
+   // it is template :class
+   // (User defined datatype)
+   student a, b, c; // a,b,c are objects of the particular class
+   vehicle c, d, e;
+}
