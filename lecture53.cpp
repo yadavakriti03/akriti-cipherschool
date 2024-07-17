0ee457d15ca39e1532fd8f44db573c5927acefec
yadavakriti03 <120010936+yadavakriti03@users.noreply.github.com>
2024-07-17 18:55:26 +0530

    Create lecture53.cpp

---
+++ lecture53.cpp
@@ -0,0 +1,12 @@
+// data structures
+// A data structure is a storage that is used to store and organize data.It is a way of arranging data on a computer so that it can be accessed and updated efficiently.
+// Algorithm	A set of step-by-step instructions to solve a specific problem.
+Arrays
+    //  An array is a data structure used to store multiple elements.
+    //  linked list
+    //      A linked list consists of nodes with some sort of data,
+    //  and a pointer, or link, to the next node.
+    //  StackLIFO
+    //  Queue FIFO
+    //  Tree hierachical structure with nodes connected by edges
+    //  Hash table stores key value pairs and provides efficient key lookup
