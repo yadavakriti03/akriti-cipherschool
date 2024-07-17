0883223662f7be8a02a38d4c245878db5a4f4610
yadavakriti03 <120010936+yadavakriti03@users.noreply.github.com>
2024-07-17 18:55:54 +0530

    Create lecture54.cpp

---
+++ lecture54.cpp
@@ -0,0 +1,2 @@
+// linked list
+A linked list is a fundamental data structure in computer science.It consists of nodes where each node contains data and a reference(link) to the next node in the sequence. This allows for dynamic memory allocation and efficient insertion and deletion operations compared to arrays.
