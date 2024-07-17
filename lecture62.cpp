4fdb3cd41ca4332638f8c01381ba1c7ab80bebc4
yadavakriti03 <120010936+yadavakriti03@users.noreply.github.com>
2024-07-17 19:01:43 +0530

    Create lecture62.cpp

---
+++ lecture62.cpp
@@ -0,0 +1,4 @@
+// mergesort
+//  Merge sort is a sorting algorithm that follows the divide-and-conquer approach. It works by recursively dividing the input array into smaller subarrays and sorting those subarrays then merging them back together to obtain the sorted array.
+
+// In simple terms, we can say that the process of merge sort is to divide the array into two halves, sort each half, and then merge the sorted halves back together. This process is repeated until the entire array is sorted.
