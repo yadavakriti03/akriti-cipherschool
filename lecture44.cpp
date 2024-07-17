442c13bc0cefc08616f6fce15398440cb71a49c9
yadavakriti03 <120010936+yadavakriti03@users.noreply.github.com>
2024-07-17 18:43:30 +0530

    Create lecture44.cpp

---
+++ lecture44.cpp
@@ -0,0 +1,20 @@
+c1x  A1 this is better algorithm 
+c2x^2 A2 
+x^2 >>>>>>x :: as size of x increses 
+criteria for comparing find the no of executable code  eg kn1+k2(n-1)n/2 to find the value 
+asymptotic analysis used 
+Asymptotic notation is a way to describe the running time or space complexity of an algorithm based on the input size. It is commonly used in complexity analysis to describe how an algorithm performs as the size of the input grows. The three most commonly used notations are Big O, Omega, and Theta.
+"higher order increasing term " 10^5x<10^-2 x^2
+n2<n3 
+n2 is more efficient 
+
+
+Big O notation (O): This notation provides an upper bound on the growth rate of an algorithm’s running time or space usage. It represents the worst-case scenario, i.e., the maximum amount of time or space an algorithm may need to solve a problem. For example, if an algorithm’s running time is O(n), then it means that the running time of the algorithm increases linearly with the input size n or less.
+f(n)≤c.g(n) for all n≥no
+
+
+Omega notation (?): This notation provides a lower bound on the growth rate of an algorithm’s running time or space usage. It represents the best-case scenario, i.e., the minimum amount of time or space an algorithm may need to solve a problem. For example, if an algorithm’s running time is ?(n), then it means that the running time of the algorithm increases linearly with the input size n or more.
+f(n)>=c.g(n) for all n≥no and c>0
+
+Theta notation (?):
+This notation provides both an upper and lower bound on the growth rate of an algorithm’s running time or space usage.It represents the average - case scenario, i.e., the amount of time or space an algorithm typically needs to solve a problem.For example, if an algorithm’s running time is ? (n), then it means that the running time of the algorithm increases linearly with the input size n.c1.g(n) <= f(n) <= c2.g(n)
