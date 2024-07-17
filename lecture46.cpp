3b5e9fc79ab197ddf73942bb91523ddbfa750109
yadavakriti03 <120010936+yadavakriti03@users.noreply.github.com>
2024-07-17 18:44:23 +0530

    Create lecture46.cpp

---
+++ lecture46.cpp
@@ -0,0 +1,12 @@
+selection sort O(n ^ 2)
+    // Applications of Greedy Algorithm
+    There are many applications of the greedy method in DAA.Some important greedy algorithm applications are :
+
+    Assigning tasks to resources to minimize waiting time
+    or maximize efficiency.Selecting the most valuable items to fit into a knapsack with limited capacity.Dividing an image into regions with similar characteristics.Reducing the size of data by removing redundant information
+
+           // Disadvantages/Limitations of Using a Greedy Algorithm
+           Below are some disadvantages of the Greedy Algorithm :
+
+    Greedy algorithms may not always find the best possible solution.The order in which the elements are considered can significantly impact the outcome.Greedy algorithms focus on local optimizations
+           and may miss better solutions that require considering a broader context.Greedy algorithms are not applicable to problems where the greedy choice does not lead to an optimal solution.
