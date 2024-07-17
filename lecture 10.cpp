0199cfe1fbbcba28bb753505d5949df5cd52753e
yadavakriti03 <120010936+yadavakriti03@users.noreply.github.com>
2024-07-17 14:36:11 +0530

    Create lecture 10.cpp

---
+++ lecture 10.cpp
@@ -0,0 +1,11 @@
+In this lecture : Learnt about the increments (post and pre) and how to execute the code blocks in for loop.in for loop there
+the synatx is for(intialization,condition,update). 
+
+#include<iostream>
+using namespace std;
+int main(){
+	for(int i=0; i<=100;i++){ //here by using the for loop print 100 numbers
+		cout<<i<<endl;
+	}
+	return 0;
+}
