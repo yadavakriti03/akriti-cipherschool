aadd724e190816d5c41d58f69fe444f4d34dcf3f
yadavakriti03 <120010936+yadavakriti03@users.noreply.github.com>
2024-07-17 14:34:54 +0530

    Create lecture 8.cpp

---
+++ lecture 8.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int array[3]={12,14,15}; // Declare the array and Access an array element by referring to the index number.
+	cout<< array[1];
+	return 0;
+}
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a[4] = {1,2,3,4}; //Declare the array and access the array element by using with for loops.
+  for (int i = 0; i < 4; i++) {
+    cout << a[i] << endl;
+  }
+  return 0;
+}
+
+#include <iostream>
+using namespace std;
+int main(){
+	int a[3] = {1,2,3};
+	for(int i=0;i<3; i++){
+		cout<<i<<"="<<a[i]<<endl;
+	}
+	return 0;
+}
+
