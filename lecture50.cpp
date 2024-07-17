1ca80feae8e7d66adfb9f866a5b4d27b97f1d84f
yadavakriti03 <120010936+yadavakriti03@users.noreply.github.com>
2024-07-17 18:46:07 +0530

    Create lecture50.cpp

---
+++ lecture50.cpp
@@ -0,0 +1,15 @@
+// CLASS and OBJECTS
+#include <iostream>
+using namespace std;
+int main()
+{
+
+   // these were class comes when you have lots of data to store that have same properties
+   //  string name;
+   //  int mid;
+   //  float ends;
+   //  bool pre_abs;
+
+   // it is template :class
+   // (User defined datatype)
+}
