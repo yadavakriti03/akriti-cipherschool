a60beff21a71d7bf28ffc66e6d0080d8fbf8a3ff
yadavakriti03 <120010936+yadavakriti03@users.noreply.github.com>
2024-07-17 18:38:07 +0530

    Create lecture37.cpp

---
+++ lecture37.cpp
@@ -0,0 +1,4 @@
+// selection sort
+//  sorting means arranging the elements in an ascd/desc order
+// you first select the least no and put in the first place and then do same for remaining elements put in the second element
+//  Selection sort is a simple and efficient sorting algorithm that works by repeatedly selecting the smallest (or largest) element from the unsorted portion of the list and moving it to the sorted portion of the list.
