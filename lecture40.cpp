38c74d41e6c28ffcd23117913d321aefafb26670
yadavakriti03 <120010936+yadavakriti03@users.noreply.github.com>
2024-07-17 18:41:38 +0530

    Update lecture40.cpp

--- lecture40.cpp
+++ lecture40.cpp
@@ -1,54 +1,34 @@
+// Insertion sort
+//  Insertion sort is a simple sorting algorithm that works by iteratively inserting each element of an unsorted list into its correct position in a sorted portion of the list.
 #include <iostream>
 using namespace std;
+int main()
+{
+   cout << "Input an array of elements" << endl;
+   int i;
+   int arr[6];
+   for (i = 0; i < 6; i++)
+      cin >> arr[i];
 
-int main(){
-    int n;
-    cout << "Enter array size: " << endl;
-    cin >> n;
-    int arr[n];
-    cout << "Enter array elements: " << endl;
-    for(int i = 0; i < n; i++){
-        cin >> arr[i];
-    }
+   int key, j;
+   for (i = 1; i < 6; i++)
+   {
+      key = arr[i];
+      j = i - 1;
 
-    // Ascending order insertion sort
-    int key;
-    for(int i = 1; i < n; i++){
-        key = arr[i];
-        int j = i - 1;
-
-        while(arr[j] > key && j >= 0){
-            arr[j + 1] = arr[j];
-            j--;
-        }
-        arr[j + 1] = key;
-    }
-    cout << "Array sorted in ascending order: " << endl;
-    for(int i = 0; i < n; i++){
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-
-    // Descending order insertion sort
-    int arr1[n];
-    cout << "Enter array elements again for descending order: " << endl;
-    for(int i = 0; i < n; i++){
-        cin >> arr1[i];
-    }
-    int key1;
-    for(int i = 1; i < n; i++){
-        key1 = arr1[i];
-        int j = i - 1;
-        while(arr1[j] < key1 && j >= 0){
-            arr1[j + 1] = arr1[j];
-            j--;    
-        }
-        arr1[j + 1] = key1;
-    }
-    cout << "Array sorted in descending order: " << endl;
-    for(int i = 0; i < n; i++){
-        cout << arr1[i] << " ";
-    }
-
-    return 0;
+      // Move elements of arr[0..i-1],
+      // that are greater than key,
+      // to one position ahead of their
+      // current position
+      while (j >= 0 && arr[j] > key)
+      {
+         arr[j + 1] = arr[j];
+         j = j - 1;
+      }
+      arr[j + 1] = key;
+   }
+   for (int i = 0; i < 6; i++)
+   {
+      cout << arr[i] << " ";
+   }
 }
