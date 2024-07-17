1ec43422c3beed3d54fbb846d7f135bc8f253fd8
yadavakriti03 <120010936+yadavakriti03@users.noreply.github.com>
2024-07-17 18:42:00 +0530

    Create lecture41.cpp

---
+++ lecture41.cpp
@@ -0,0 +1,11 @@
+// algorithm analysis
+// efficient algo
+// insertion sort  vs  selection sort
+//  Algorithm analysis is an important part of computational complexity theory, which provides theoretical estimation for the required resources of an algorithm to solve a specific computational problem. Analysis of algorithms is the determination of the amount of time and space resources required to execute it.
+//  To predict the behavior of an algorithm without implementing it on a specific computer.
+//  It is much more convenient to have simple measures for the efficiency of an algorithm than to implement the algorithm and test the efficiency every time a certain parameter in the underlying computer system changes.
+//  It is impossible to predict the exact behavior of an algorithm. There are too many influencing factors.
+//  The analysis is thus only an approximation; it is not perfect.
+//  More importantly, by analyzing different algorithms, we can compare them to determine the best one for our purpose.
+
+// we can compare based on no of lines of codes
