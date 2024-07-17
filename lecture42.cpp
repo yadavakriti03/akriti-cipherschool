138f38167ddaf668a824e8810962aa0109df6bcd
yadavakriti03 <120010936+yadavakriti03@users.noreply.github.com>
2024-07-17 18:42:25 +0530

    Create lecture42.cpp

---
+++ lecture42.cpp
@@ -0,0 +1,21 @@
+// the no of times each line gets executed in the psedu code
+
+// each line is statement less no of code/statement will take less time to executed
+
+// insertion sort
+//  for j=2 to A.length
+//      key=A[j]
+//      //insert A[j] into the sorted
+//          sequence A[1..j-1]
+//        i=j-1
+//        while i>0 and A[i]key
+//           A[i+1]=A[j]
+//           i=i-1
+//        A[i+1]=key
+
+// types of analysis algo
+// best case /worst case /avg case
+// Best case: Define the input for which algorithm takes less time or minimum time. In the best case calculate the lower bound of an algorithm. Example: In the linear search when search data is present at the first location of large data then the best case occurs.
+// Worst Case: Define the input for which algorithm takes a long time or maximum time. In the worst calculate the upper bound of an algorithm. Example: In the linear search when search data is not present at all then the worst case occurs.
+// Average case: In the average case take all random inputs and calculate the computation time for all inputs.
+// And then we divide it by the total number of inputs.
