574d65899a25e2e67728aa505e82e0aa5ce0ee49
yadavakriti03 <120010936+yadavakriti03@users.noreply.github.com>
2024-07-17 14:33:57 +0530

    Create lecture 7.cpp

---
+++ lecture 7.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a,b,c; // reverse the numbers 
+	cin>>a>>b>>c;
+	cout<<"print the numbers in reverse order "<<c<<b<<a<<endl;
+	return 0;
+}
+
+// Declare the array 
+#include <iostream>
+using namespace std;
+int main(){
+	int array[5];
+	
+	return 0;
+}
